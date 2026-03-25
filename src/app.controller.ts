a2c3adc06b28b84626c246e40f27206bdc00cea0
68010417 <68010417@kmitl.ac.th>
2026-03-25 10:16:26 +0700

    p00_2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiResponse } from './interfaces/response.interface';
+
+interface IStatus {
+  service: string;
+  version: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): ApiResponse<IStatus> {
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { ApiResponse } from './interfaces/response.interface';
+
+interface IStatus {
+  service: string;
+  version: string;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello NestJS';
+  getHello(): ApiResponse<IStatus> {
+    const statusResponse: ApiResponse<IStatus> = {
+      success: true,
+      data: {
+        service: 'purchase-api',
+        version: '1.0.0',
+      },
+      message: 'Hello NestJS',
+    };
+
+    return statusResponse;
   }
 }
